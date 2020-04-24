e0ddc6c6e9bba9b6267ab8db80fd45eedb5e346e
alipinski15 <58602252+alipinski15@users.noreply.github.com>
2020-04-23 17:01:29 -0700

    update

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,11 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+ class Game {
+    constructor(){
+        this.missed = 0;
+        this.phrases = ["Carpe Diem","Oh captain my captain","Just keep swimming","Youre killin me Smalls","Hang in there"];
+        this.activePhrase = null;
+    }
+ }
